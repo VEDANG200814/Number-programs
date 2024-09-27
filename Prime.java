e6bdd198d19e0dbb2b834906efb665fa73f41ed1
Vedang Chakraborty <91678229+VEDANG200814@users.noreply.github.com>
2024-09-27 21:43:40 +0530

    fixes

--- Prime.java
+++ Prime.java
@@ -5,15 +5,17 @@ public class Prime {
         System.out.println("Enter a number");
         int n=sc.nextInt();
         int c=0;
-        for(int i=1;i<=n;i++)//for(int i=1;i<=(int)Math.sqrt(n);i++)
-        if(n%i==0)//{
-        // if(i==1||i==n)
-        // c++;
-        // else
-        // c+=2;
-        //}
+        // int f=1;
+        for(int i=1;i<=n;i++)
+        // for(int i=2;i<=(int)Math.sqrt(n);i++)
+        if(n%i==0)
+        // {
+            // f=0;
+            // break;
+        // }
         c++;
         if(c==2)
+        // if(f==1)
         System.out.println(n+" is a prime number");
         else
         System.out.println(n+" is not a prime number");
