fdf087e0134d5e241febd6bfdc6838f389aaf262
Vedang Chakraborty <91678229+VEDANG200814@users.noreply.github.com>
2024-12-25 13:54:57 +0530

    Update Cyclic.java

--- Cyclic.java
+++ Cyclic.java
@@ -1,11 +1,26 @@
 import java.util.*;
-public class Cyclic {
-    long n;
-    Cyclic(long x){
-        n=x;
-    }
-    void check(){
-        String x=String.valueOf(n);
-        
+public class Sameproduct {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt(),n1=n,nd=0,f=1,i1=0;
+        while(n1>0){
+            nd++;
+            n1=n1/10;
+        }
+        for(int i=1;i<=nd;i++){
+            i1=i*n;
+            while (i1!=0){
+                if(String.valueOf(n).indexOf(String.valueOf(i1%10))==-1){
+                    f=0;
+                    break;
+                }
+                i1/=10;
+            }
+        }
+        if(f==1)
+        System.out.println("The number "+n+" is a Cyclic number");
+        else
+        System.out.println("The number "+n+" is not a Cyclic number");
     }
 }
